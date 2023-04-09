9f23d8af9906334aa1eb2c8bd804ec83284924ab
BennyHung <bennyhudsonhung@gmail.com>
2023-04-08 22:48:14 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+function writeCards(arrayOfNames,event){
+    let newArray = []
+    for (let i=0; i < arrayOfNames.length; i++){
+        let phrase = `Thank you, ${arrayOfNames[i]}, for the wonderful ${event} gift!`
+        newArray.push(phrase)
+    }
+    console.log(newArray)
+    return newArray
+}
+
+let team = ["harry","miles","david","katie"]
+
+writeCards(team, "birthday")
+
+
+function countDown(int){
+    while (int >= 0){
+        console.log(int)
+        int--
+    }
+}
+
+countDown(10)
